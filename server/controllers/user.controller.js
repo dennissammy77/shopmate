d08f15f51c671a600c1bce2644f71e82599d8dff
Dennis Sammy <dennissammy77@gmail.com>
2025-05-03 00:10:05 +0300

    feat(user): add PUT /api/users/me for updating name and preferences

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,4 +11,24 @@ router.get('/me', async (req, res) => {
   }
 });
 
+router.put('/me', async (req, res) => {
+  const { name, preferences } = req.body;
+
+  try {
+    const updates = {};
+    if (name) updates.name = name;
+    if (preferences) updates.preferences = preferences;
+
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { $set: updates },
+      { new: true, runValidators: true }
+    ).select('-passwordHash');
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 module.exports = router;
