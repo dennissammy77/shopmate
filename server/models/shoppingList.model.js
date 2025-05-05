c8c2bfc539d11f46daa8e570075636c867142c45
Dennis Sammy <dennissammy77@gmail.com>
2025-05-05 14:10:47 +0300

    schema: add item to shopping list schema

--- server/models/shoppingList.model.js
+++ server/models/shoppingList.model.js
@@ -17,6 +17,41 @@ const ShoppingListSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
+    items: [
+      {
+        name: String,
+        quantity: Number,
+        status: {
+          type: String,
+          enum: ['pending', 'purchased'],
+          default: 'pending',
+        },
+        lastModifiedBy: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+        priceInfo: {
+          storeName: String,
+          price: Number,
+        },
+        history: [
+          {
+            action: {
+              type: String,
+              enum: ['add', 'edit', 'delete', 'purchase'],
+            },
+            timestamp: {
+              type: Date,
+              default: Date.now,
+            },
+            userId: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'User',
+            },
+          },
+        ],
+      },
+    ],
   },
   { timestamps: true }
 );
