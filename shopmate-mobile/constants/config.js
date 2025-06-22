fd0d620a9b481e90328024fb9a5a0c1bb8eb27bc
dennissammy77 <dennissammy77@gmail.com>
2025-06-22 15:51:47 +0300

    ip variable

---
+++ shopmate-mobile/constants/config.js
@@ -0,0 +1,5 @@
+// export const API_URL = 'https://shopmate-ue43.onrender.com'
+//  export const API_URL = 'http://jcocs8g80oso4os8s8ok0ccg.46.202.129.210.sslip.io'
+// const IP_ADDR = '192.168.100.6'
+const IP_ADDR = '172.20.224.1'
+export const API_URL = `http://${IP_ADDR}:3000` 
